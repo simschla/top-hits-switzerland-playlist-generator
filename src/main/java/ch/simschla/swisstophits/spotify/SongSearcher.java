ec9bc393e88df87a8abffa9cbbbae915778b29e6
Simon Gamma <github@survive.ch>
2022-06-28 10:48:33 +0200

    retry on not found

--- src/main/java/ch/simschla/swisstophits/spotify/SongSearcher.java
+++ src/main/java/ch/simschla/swisstophits/spotify/SongSearcher.java
@@ -8,15 +8,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import se.michaelthelin.spotify.SpotifyApi;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+import se.michaelthelin.spotify.exceptions.detailed.NotFoundException;
 import se.michaelthelin.spotify.model_objects.specification.Paging;
 import se.michaelthelin.spotify.model_objects.specification.Track;
 
 import java.io.IOException;
 import java.text.Normalizer;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -33,33 +31,53 @@ public class SongSearcher {
 
 
     public List<Track> search(@NonNull SongInfo songInfo) {
-        Paging<Track> trackPaging = null;
+        final String searchString = searchString(songInfo);
+        LOGGER.info("({}) Searching with '{}' for {}", songInfo.getPosition(), searchString, songInfo.toShortDesc());
+        List<Track> tracks = search(searchString);
+        if (tracks.isEmpty()) {
+            final String searchStringReduced = searchStringReduced(songInfo);
+            LOGGER.info("({}) Nothing found -- Searching with '{}' for {}", songInfo.getPosition(), searchStringReduced, songInfo.toShortDesc());
+            tracks = search(searchStringReduced);
+        }
+        return tracks;
+    }
+
+    private List<Track> search(@NonNull String searchString) {
         try {
-            final String searchString = searchString(songInfo);
-            LOGGER.info("({}) Searching with '{}' for {}", songInfo.getPosition(), searchString, songInfo.toShortDesc());
-            trackPaging = this.spotifyApi.searchTracks(searchString)
+            Paging<Track> trackPaging = this.spotifyApi.searchTracks(searchString)
                     .market(CountryCode.CH)
                     .build()
                     .execute();
             return new ArrayList<>(Arrays.asList(trackPaging.getItems()));
+        } catch (NotFoundException e) {
+            return Collections.emptyList();
         } catch (IOException | SpotifyWebApiException | ParseException e) {
             throw new SpotifyException(e);
         }
     }
 
     private static String searchString(@NonNull SongInfo songInfo) {
+        return searchString(Stream.of(
+                Stream.of("track:" + songInfo.getSong()),
+                replaceSpecialArtists(songInfo).map(artist -> "artist:" + artist),
+                Stream.of("year:" + (songInfo.getChartYear() - 1) + "-" + (songInfo.getChartYear()))
+        ));
+    }
+
+    private static String searchStringReduced(@NonNull SongInfo songInfo) {
+        return searchString(Stream.of(
+                Stream.of("track:" + songInfo.getSong()),
+                replaceSpecialArtists(songInfo).limit(2).map(artist -> "artist:" + artist)
+        ));
+    }
+
+    private static String searchString(Stream<Stream<String>> searchStreams) {
         return Normalizer.normalize(
-                        Stream.of(
-                                        Stream.of("track:" + songInfo.getSong()),
-                                        replaceSpecialArtists(songInfo).map(artist -> "artist:" + artist),
-                                        Stream.of("year:" + (songInfo.getChartYear() - 1) + "-" + (songInfo.getChartYear()))
-//                                        Stream.of(String.valueOf(songInfo.getChartYear()))
-                                )
+                        searchStreams
                                 .flatMap(s -> s)
                                 .map(String::trim)
                                 .map(s -> "".equals(s) ? null : s)
                                 .filter(Objects::nonNull)
-//                .map(String::toLowerCase)
                                 .collect(Collectors.joining(" ")),
                         Normalizer.Form.NFKD)
                 .replaceAll("[^\\p{ASCII}]", "");
