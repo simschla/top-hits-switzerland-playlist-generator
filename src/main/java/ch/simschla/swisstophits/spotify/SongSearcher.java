79c266e203f5e510627307a024b2997065c5fae9
Simon Gamma <github@survive.ch>
2023-01-04 08:02:11 +0100

    searching: Multi-Level searching

--- src/main/java/ch/simschla/swisstophits/spotify/SongSearcher.java
+++ src/main/java/ch/simschla/swisstophits/spotify/SongSearcher.java
@@ -35,9 +35,29 @@ public class SongSearcher {
         LOGGER.info("({}) Searching with '{}' for {}", songInfo.getPosition(), searchString, songInfo.toShortDesc());
         List<Track> tracks = search(searchString);
         if (tracks.isEmpty()) {
-            final String searchStringReduced = searchStringReduced(songInfo);
-            LOGGER.info("({}) Nothing found -- Searching with '{}' for {}", songInfo.getPosition(), searchStringReduced, songInfo.toShortDesc());
-            tracks = search(searchStringReduced);
+            final String searchStringWithoutArtistTags = searchStringWithoutArtistTags(songInfo);
+            LOGGER.info("({}) Nothing found -- Searching with '{}' for {}", songInfo.getPosition(), searchStringWithoutArtistTags, songInfo.toShortDesc());
+            tracks = search(searchStringWithoutArtistTags);
+        }
+        if (tracks.isEmpty()) {
+            final String searchStringWithoutYearTag = searchStringWithoutYearTag(songInfo);
+            LOGGER.info("({}) Nothing found (2) -- Searching with '{}' for {}", songInfo.getPosition(), searchStringWithoutYearTag, songInfo.toShortDesc());
+            tracks = search(searchStringWithoutYearTag);
+        }
+        if (tracks.isEmpty()) {
+            final String searchStringWithoutYearAndArtistTags = searchStringWithoutYearAndArtistTags(songInfo);
+            LOGGER.info("({}) Nothing found (3) -- Searching with '{}' for {}", songInfo.getPosition(), searchStringWithoutYearAndArtistTags, songInfo.toShortDesc());
+            tracks = search(searchStringWithoutYearAndArtistTags);
+        }
+        if (tracks.isEmpty()) {
+            final String searchStringWithoutTrackAndArtistTags = searchStringWithoutTrackAndArtistTags(songInfo);
+            LOGGER.info("({}) Nothing found (4) -- Searching with '{}' for {}", songInfo.getPosition(), searchStringWithoutTrackAndArtistTags, songInfo.toShortDesc());
+            tracks = search(searchStringWithoutTrackAndArtistTags);
+        }
+        if (tracks.isEmpty()) {
+            final String searchStringWithoutTags = searchStringWithoutTags(songInfo);
+            LOGGER.info("({}) Nothing found (5) -- Searching with '{}' for {}", songInfo.getPosition(), searchStringWithoutTags, songInfo.toShortDesc());
+            tracks = search(searchStringWithoutTags);
         }
         return tracks;
     }
@@ -58,16 +78,46 @@ public class SongSearcher {
 
     private static String searchString(@NonNull SongInfo songInfo) {
         return searchString(Stream.of(
-                Stream.of("track:" + songInfo.getSong()),
-                replaceSpecialArtists(songInfo).map(artist -> "artist:" + artist),
+                Stream.of(String.format("track:\"%s\"", songInfo.getSong())),
+                replaceSpecialArtists(songInfo).map(artist -> String.format("artist:\"%s\"", artist)),
                 Stream.of("year:" + (songInfo.getChartYear() - 1) + "-" + (songInfo.getChartYear()))
         ));
     }
 
-    private static String searchStringReduced(@NonNull SongInfo songInfo) {
+    private static String searchStringWithoutArtistTags(@NonNull SongInfo songInfo) {
         return searchString(Stream.of(
-                Stream.of("track:" + songInfo.getSong()),
+                replaceSpecialArtists(songInfo).map(artist -> String.format("\"%s\"", artist)),
+                Stream.of(String.format("track:\"%s\"", songInfo.getSong())),
+                Stream.of("year:" + (songInfo.getChartYear() - 1) + "-" + (songInfo.getChartYear()))
+        ));
+    }
+
+    private static String searchStringWithoutTrackAndArtistTags(@NonNull SongInfo songInfo) {
+        return searchString(Stream.of(
+                replaceSpecialArtists(songInfo).map(artist -> String.format("\"%s\"", artist)),
+                Stream.of(String.format("\"%s\"", songInfo.getSong())),
+                Stream.of("year:" + (songInfo.getChartYear() - 1) + "-" + (songInfo.getChartYear()))
+        ));
+    }
+
+    private static String searchStringWithoutTags(@NonNull SongInfo songInfo) {
+        return searchString(Stream.of(
+                replaceSpecialArtists(songInfo).map(artist -> String.format("\"%s\"", artist)),
+                Stream.of(String.format("\"%s\"", songInfo.getSong()))
+        ));
+    }
+
+    private static String searchStringWithoutYearTag(@NonNull SongInfo songInfo) {
+        return searchString(Stream.of(
+                Stream.of(String.format("track:\"%s\"", songInfo.getSong())),
                 replaceSpecialArtists(songInfo).limit(2).map(artist -> "artist:" + artist)
+        ));
+    }
+
+    private static String searchStringWithoutYearAndArtistTags(@NonNull SongInfo songInfo) {
+        return searchString(Stream.of(
+                replaceSpecialArtists(songInfo).limit(2).map(artist -> String.format("\"%s\"", artist)),
+                Stream.of(String.format("track:\"%s\"", songInfo.getSong()))
         ));
     }
 
